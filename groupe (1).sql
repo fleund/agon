75d0be36b166a7c6dfca44a794c826b38e6e2f43
fleund <come.cothenet@hotmail.fr>
2016-05-03 16:12:01 +0200

    Update groupe (1).sql

--- groupe (1).sql
+++ groupe (1).sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  127.0.0.1
--- Généré le :  Ven 29 Avril 2016 à 13:03
+-- GÃ©nÃ©rÃ© le :  Mar 03 Mai 2016 Ã  14:11
 -- Version du serveur :  5.7.9
 -- Version de PHP :  5.6.15
 
@@ -17,7 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données :  `agon`
+-- Base de donnÃ©es :  `agon`
 --
 
 -- --------------------------------------------------------
@@ -31,12 +31,22 @@ CREATE TABLE IF NOT EXISTS `groupe` (
   `nom_groupe` varchar(50) NOT NULL,
   `description_groupe` text,
   `nombre_max_membres` int(11) NOT NULL DEFAULT '100',
-  `statut` varchar(6) NOT NULL DEFAULT 'public' COMMENT 'public=0 privé=1',
+  `statut` varchar(6) NOT NULL DEFAULT 'public' COMMENT 'public=0 privÃ©=1',
+  `image_groupe` varchar(100) DEFAULT NULL COMMENT 'lien de l''image du groupe',
   `id_groupe` int(11) NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (`id_groupe`),
   UNIQUE KEY `nom_groupe` (`nom_groupe`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin7;
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin7;
+
+--
+-- Contenu de la table `groupe`
+--
+
+INSERT INTO `groupe` (`nom_groupe`, `description_groupe`, `nombre_max_membres`, `statut`, `image_groupe`, `id_groupe`) VALUES
+('nom ici', 'Description du groupe ici ', 140, 'public', NULL, 1),
+('nom groupe privÃ© 2', 'ici c''est un groupe privÃ©', 25, 'prive', NULL, 2);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
