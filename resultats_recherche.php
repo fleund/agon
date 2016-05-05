322f84a4ac0a6a5a5746adfbf03481ccba8050af
NicolasTYSSANDIER <nicolas_881@hotmail.fr>
2016-05-06 01:41:05 +0200

    Create resultats_recherche.php

---
+++ resultats_recherche.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+
+<?php include('bdd.php') ?>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Résultats de la recherche</title>
+        <link rel="stylesheet" href="Accueil.css">
+    </head>
+
+    <body>
+    	<h1>Résultats de la recherche</h1></br></br>
+    	<?php include('search.php') ?>
+    </body>
+</html>
