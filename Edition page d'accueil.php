0ac2f34effb68c9cc303f900730aab2629cae2c4
allancerol <allancerol@gmail.com>
2016-05-06 02:08:04 +0200

    Update Edition page d'accueil.php

--- Edition page d'accueil.php
+++ Edition page d'accueil.php
@@ -1,69 +1,44 @@
 <!DOCTYPE html>
-
-<?php include('bdd.php') ?>
-
 <html>
     
     <head>
             <meta charset="utf-8">
-            <title>Édition de la page d'accueil</title>
+            <title>Ã‰dition de la page d'accueil</title>
             <link rel="stylesheet" href="Edition page d'accueil.css" />
     </head>
 
-    <header>
-        <img class="Logo" src="Logo.png" alt="Logo ISEP/Agon" />
-        <p id="onglets_accueil">
-        	<a href="">Accueil</a> |
-        	<a href="">Forums</a> |
-        	<a href="">Groupes</a> |
-        	<a href="">Mon espace</a> |
-        	<a href="">Déconnexion</a> |
-        </p>
-        <div id="identifiants">
-            <p id="nom_utilisateur"> Nom d'utilisateur  </p>
-            <form>
-    	      	<input type="text" name="Nom d'utilisateur" /> <br/>
-            </form>
-            <p id="mot_passe"> Mot de passe  </p>
-            <form>
-                <input type="password" name="Nom d'utilisateur" /> <br/>
-            </form>
-            <button type="button" onclick="alert('Se connecter');"> 
-                Se connecter 
-            </button>
-            &nbsp;&nbsp;
-            <a href="">Mot de passe oublié ? </a>
-        </div>
-    </header>
+   
 
     <body>
-        <div id="Corps">
-            <div id="barre_bleue_gauche"></div>
-            <div id="barre_bleue_droite"></div>
-            <div id="barre_mauve_gauche"></div>
-            <div id="barre_mauve_droite"></div>
-	    </div>
         
-        <p class = "titre">Édition de la page d'accueil</p>
+        <p class = "titre">Ã‰dition de la page d'accueil</p>
 
-        <?php // Mise à jour de la base de données, suite au remplissage des champs
+        <?php // Mise Ã  jour de la base de donnÃ©es, suite au remplissage des champs
             if (isset($_GET['modifications'])) {
+                try {$bdd = new PDO('mysql:host=localhost;dbname=agon;charset=utf8', 'root', '');}
+                catch (Exception $e) {die('Erreur : ' . $e->getMessage());}
                 for ($i = 1; $i <= 16; $i++) {
-                    $req = $bdd->prepare('UPDATE sports_en_tete SET nom=:tmp1 WHERE ID = :i'); // Table de la liste des sports en en-tête
+                    $req = $bdd->prepare('UPDATE sports_en_tete SET nom=:tmp1 WHERE ID = :i'); // Table de la liste des sports en en-tÃªte
                     $req->execute(array(
                         'tmp1' => $_POST['en_tete' . $i],
                         'i' => $i
                     ));
+                    $req = $bdd->prepare('UPDATE liste_compets SET nom=:tmp2 WHERE ID = :i'); // Table de la liste des compÃ©titions
+                    $req->execute(array(
+                        'tmp2' => $_POST['compet' . $i],
+                        'i' => $i
+                    ));
                 }
                 $reponse = $bdd->query('SELECT * FROM liste_sports ORDER BY nom');
-                while($donnees = $reponse->fetch()) { // On teste si le sport à ajouter est déjà dans la liste
+                while($donnees = $reponse->fetch()) { // On teste si le sport Ã  ajouter est dÃ©jÃ  dans la liste
                     if ($donnees['nom']==$_POST['ajout_sport']) {
                         $doublon=True;
-                        echo '</br><strong class="erreur">Le sport "' . $_POST['ajout_sport'] . '" est déjà dans la liste des sports. Il n\'a donc pas été ajouté.</strong></br>';
+                        echo '</br><strong class="erreur">Le sport "' . $_POST['ajout_sport'] . '" est dÃ©jÃ  dans la liste des sports. Il n\'a donc pas Ã©tÃ© ajoutÃ©.</strong></br>';
                     }
                 }
                 if (!isset($doublon)) {
-                    $req = $bdd->prepare('INSERT INTO liste_sports(nom) VALUES (:tmp)'); // On ajoute le sport à la liste globale
+                    $reponse->closeCursor();
+                    $req = $bdd->prepare('INSERT INTO liste_sports(nom) VALUES(:tmp)'); // On ajoute le sport Ã  la liste globale
                     $req->execute(array(
                         'tmp' => $_POST['ajout_sport']
                     ));
@@ -72,38 +47,57 @@
                 $req->execute(array(
                     'tmp' => $_POST['suppr_sport']
                 ));
-                $req = $bdd->prepare('UPDATE sports_en_tete SET nom=\'\' WHERE nom=:tmp'); // Tant qu'à faire, on supprime aussi ce sport de l'en-tête
+                $req = $bdd->prepare('UPDATE sports_en_tete SET nom=\'\' WHERE nom=:tmp'); // Tant qu'Ã  faire, on supprime aussi ce sport de l'en-tÃªte
                 $req->execute(array(
                     'tmp' => $_POST['suppr_sport']
                 ));
                 echo '<strong>Les ';
                 if (isset($doublon)) {echo 'autres ';}
-                echo 'modifications ont été enregistrées.</strong></br></br>';
+                echo 'modifications ont Ã©tÃ© enregistrÃ©es.</strong></br></br>';
             }
         ?>
 
         <form action="Edition page d'accueil.php?modifications=oui" method="post">
-            <fieldset class="bloc">
-                <legend>Modifier la liste des sports en en-tête de la page d'accueil</legend>
-                </br><em>Note : ces champs déterminent également l'ordre des sports dans l'en-tête.</em></br></br>
+            <div id="bloc1">
+                <p class="tb1">Modifier la liste des sports en en-tÃªte de la page d'accueil<p>
+                </br><em>Note : ces champs dÃ©terminent Ã©galement l'ordre des sports dans l'en-tÃªte.</em></br></br>
+				
+				<div id="essai">
 
-                <?php // Affichage des champs pour modifier la liste des sports en en-tête, préremplis avec la liste actuelle
+                <?php // Affichage des champs pour modifier la liste des sports en en-tÃªte, prÃ©remplis avec la liste actuelle
+                    try {$bdd = new PDO('mysql:host=localhost;dbname=agon;charset=utf8', 'root', '');}
+                    catch (Exception $e) {die('Erreur : ' . $e->getMessage());}
                     $reponse = $bdd->query('SELECT * FROM sports_en_tete ORDER BY ID');
                     for ($i = 1; $i <= 16; $i++) {
                         $donnees = $reponse->fetch();
                         echo $i . ' :&nbsp;<input type="text" name="en_tete' . $donnees['ID'] . '" id="sport" maxlength ="30" value="' . $donnees['nom'] . '">&nbsp;&nbsp;';
                     }
+                    $reponse->closeCursor();
                 ?>
+				
+				</div>
 
-            </fieldset></br></br>
+            </div></br></br>
             
             <fieldset class="bloc">
-                <legend>Modifier la liste exhaustive des sports représentés sur ce site</legend> 
+                <legend>Modifier la liste exhaustive des sports reprÃ©sentÃ©s sur ce site</legend> 
 
-                </br>Sélectionner un sport à supprimer de la liste :&nbsp;&nbsp;
-                <?php include('liste_sports.php') ?>
+                </br>SÃ©lectionner un sport Ã  supprimer de la liste :&nbsp;&nbsp;
+                <input type="text" list="choix_sport" name="suppr_sport" placeholder="Cliquez ici..."> 
+                <datalist id="choix_sport">
+                    <?php // SÃ©lection d'un sport Ã  supprimer
+                        try {$bdd = new PDO('mysql:host=localhost;dbname=agon;charset=utf8', 'root', '');}
+                        catch (Exception $e) {die('Erreur : ' . $e->getMessage());}
+                        $bdd->exec('DELETE FROM liste_sports WHERE nom=\'\''); // Y avait un bug, quand je supprimais un sport il supprimait pas la ligne mais laissait Ã  la place un champ vide, donc je supprime le champ vide pour corriger
+                        $reponse = $bdd->query('SELECT * FROM liste_sports ORDER BY nom');
+                        while($donnees = $reponse->fetch()) {
+                            echo '<option>' . $donnees['nom'] . '</option>';
+                        }
+                        $reponse->closeCursor();
+                    ?>
+                </datalist>
 
-                </br></br>Ajouter un sport à la liste :
+                </br></br>Ajouter un sport Ã  la liste :
                 <input name=ajout_sport type="text" maxlength="30">
 
             </fieldset></br></br>
@@ -118,50 +112,53 @@
                 
                 <legend >Modifier le roulement des photos</legend>
                 </br><em>L'envoi du fichier peut prendre quelques instants, merci de patienter.</br>
-                Formats acceptés : .jpg, .jpeg, .gif ou .png. Taille : max 1 Mo.</em></br>
+                Formats acceptÃ©s : .jpg, .jpeg, .gif ou .png. Taille : max 1 Mo.</em></br>
 
                 <?php // Gestion des images sur la page d'accueil
+                    try {$bdd = new PDO('mysql:host=localhost;dbname=agon;charset=utf8', 'root', '');}
+                    catch (Exception $e) {die('Erreur : ' . $e->getMessage());}
                     if (isset($_GET['supprimer']) AND isset($_GET['nom'])) { // Traitement d'une demande de suppression d'une image
                         $ancien_nom = $_GET['nom'];
                         @unlink('uploads/' . $ancien_nom);
                         $req = $bdd->prepare('UPDATE liste_photos SET nom = \'\' WHERE ID = :i');
                         $req->execute(array('i' => $_GET['supprimer']));
-                        echo '</br><strong>La photo n°' . $_GET['supprimer'] . ' (' . $ancien_nom . ') a été supprimée.</strong>';
+                        echo '</br><strong>La photo nÂ°' . $_GET['supprimer'] . ' (' . $ancien_nom . ') a Ã©tÃ© supprimÃ©e.</strong>';
                     }
                     $j=0;
                     $reponse = $bdd->query('SELECT * FROM liste_photos ORDER BY ID');
                     for ($i = 1; $i <= 10; $i++) { // Upload des photos dans le dossier "uploads"
                         $donnees = $reponse->fetch();
-                        if (isset($_FILES['photo' . $i]) AND $_FILES['photo' . $i]['error'] == 0) { // On vérifie que l'utilisateur a bien mis une photo et qu'elle a été chargée correctement
-                            if ($_FILES['photo' . $i]['size'] <= 1048576) { // On vérifie que la taille de la photo est inférieure à 1 Mo
+                        if (isset($_FILES['photo' . $i]) AND $_FILES['photo' . $i]['error'] == 0) { // On vÃ©rifie que l'utilisateur a bien mis une photo et qu'elle a Ã©tÃ© chargÃ©e correctement
+                            if ($_FILES['photo' . $i]['size'] <= 1048576) { // On vÃ©rifie que la taille de la photo est infÃ©rieure Ã  1 Mo
                                 $infosfichier = pathinfo($_FILES['photo' . $i]['name']);
                                 $extension_upload = $infosfichier['extension'];
                                 $extensions_autorisees = array('jpg', 'jpeg', 'gif', 'png');
-                                if (in_array($extension_upload, $extensions_autorisees)) { // On vérifie que l'extension de la photo est bonne
-                                    @unlink('uploads/' . $donnees['nom']); // Si il y avait déjà une photo dans ce champ, on la supprime du dossier "uploads"
-                                    move_uploaded_file($_FILES['photo' . $i]['tmp_name'], 'uploads/' . basename($_FILES['photo' . $i]['name'])); // Toutes les conditions sont vérifiées, donc on stocke la photo dans le dossier "uploads"
-                                    $req = $bdd->prepare('UPDATE liste_photos SET nom = :tmp WHERE ID = :i'); // On stocke également le nom de cette photo dans la table "liste_photos"
+                                if (in_array($extension_upload, $extensions_autorisees)) { // On vÃ©rifie que l'extension de la photo est bonne
+                                    @unlink('uploads/' . $donnees['nom']); // Si il y avait dÃ©jÃ  une photo dans ce champ, on la supprime du dossier "uploads"
+                                    move_uploaded_file($_FILES['photo' . $i]['tmp_name'], 'uploads/' . basename($_FILES['photo' . $i]['name'])); // Toutes les conditions sont vÃ©rifiÃ©es, donc on stocke la photo dans le dossier "uploads"
+                                    $req = $bdd->prepare('UPDATE liste_photos SET nom = :tmp WHERE ID = :i'); // On stocke Ã©galement le nom de cette photo dans la table "liste_photos"
                                     $req->execute(array(
                                         'tmp' => basename($_FILES['photo' . $i]['name']),
                                         'i' => $i
                                     ));
-                                    $j+=1; // on incrémente une variable qui compte le nombre de modifications effectuées
+                                    $j+=1; // on incrÃ©mente une variable qui compte le nombre de modifications effectuÃ©es
                                 }
-                                else {echo '</br><strong class="erreur">Photo n°' . $i . ' : ce format de fichier n\'est pas accepté.</strong>';}
+                                else {echo '</br><strong class="erreur">Photo nÂ°' . $i . ' : ce format de fichier n\'est pas acceptÃ©.</strong>';}
                             }
-                            else {echo '</br><strong class="erreur">Photo n°' . $i . ' : ce fichier est trop volumineux.</strong>';}
+                            else {echo '</br><strong class="erreur">Photo nÂ°' . $i . ' : ce fichier est trop volumineux.</strong>';}
                         }
                     }
-                    if ($j==1) {echo '</br><strong>1 modification enregistrée.</strong>';}
-                    if ($j>1) {echo '</br><strong>' . $j . ' modifications enregistrées.</strong>';}
+                    if ($j==1) {echo '</br><strong>1 modification enregistrÃ©e.</strong>';}
+                    if ($j>1) {echo '</br><strong>' . $j . ' modifications enregistrÃ©es.</strong>';}
                     $reponse = $bdd->query('SELECT * FROM liste_photos ORDER BY ID');
                     while ($donnees = $reponse->fetch()) {
-                        echo '</br></br>Photo n°' . $donnees['ID'] .' : <strong>' . $donnees['nom'] . '</strong>'; // Affichage du nom et de l'ID actuels de la photo
-                        if ($donnees['nom']!='') { // Si il y avait déjà une photo, affichage d'un lien qui permet de la supprimer
+                        echo '</br></br>Photo nÂ°' . $donnees['ID'] .' : <strong>' . $donnees['nom'] . '</strong>'; // Affichage du nom et de l'ID actuels de la photo
+                        if ($donnees['nom']!='') { // Si il y avait dÃ©jÃ  une photo, affichage d'un lien qui permet de la supprimer
                             echo '&nbsp;<a href="Edition page d\'accueil.php?supprimer=' . $donnees['ID'] . '&nom=' . $donnees['nom'] .'#roulement_photos" title="Supprimer cette photo"><img src="http://i84.servimg.com/u/f84/18/62/32/81/suppri10.jpg"></a>';
                         }
-                        echo '</br><input type="file" name="photo' . $donnees['ID'] . '" id="photo">'; // On affiche les champs de sélection des photos
+                        echo '</br><input type="file" name="photo' . $donnees['ID'] . '" id="photo">'; // On affiche les champs de sÃ©lection des photos
                     }
+                    $reponse->closeCursor();
                 ?>
 
             </fieldset></br></br>
