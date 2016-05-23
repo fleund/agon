e5caca76a86c34123caf4d89cc4f46725427d577
NicolasTYSSANDIER <nicolas_881@hotmail.fr>
2016-05-23 19:54:27 +0200

    Update recherche_avancee.js

--- recherche_avancee.js
+++ recherche_avancee.js
@@ -1,11 +1,18 @@
 function recherche_avancee(id) {
-    if(document.getElementById(id).style.visibility=="hidden") {
+    if (document.getElementById(id).style.visibility=="hidden") {
         document.getElementById(id).style.visibility="visible";
+        document.getElementById(id).style.height="110px";
         document.getElementById('activer').innerHTML='Désactiver la recherche avancée';
+        $(function() {
+        	$('#champ_recherche').attr('action',"resultats_recherche.php?avancee=true#search-btn");
+        });
     }
     else {
         document.getElementById(id).style.visibility="hidden";
+        document.getElementById(id).style.height="0px";
         document.getElementById('activer').innerHTML='Activer la recherche avancée';
+        $(function() {
+        	$('#champ_recherche').attr('action',"resultats_recherche.php?avancee=false#search-btn");
+        });
     }
-    return true;
 }
