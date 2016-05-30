62f2919f97c0cdf754b7325bb4ff19b2951386e5
NicolasTYSSANDIER <nicolas_881@hotmail.fr>
2016-05-30 23:00:53 +0200

    Update entrer_date.php

--- entrer_date.php
+++ entrer_date.php
@@ -2,7 +2,7 @@
 <input type="number" name="mois" maxlength="2" placeholder="MM" class="champ_date"<?php if (isset($_POST['mois'])) {echo ' value="' . $_POST['mois'] . '" ';} ?>> / 
 <input type="number" name="annee" maxlength="4" placeholder="AAAA" class="champ_date"<?php if (isset($_POST['annee'])) {echo ' value="' . $_POST['annee'] . '" ';} ?>>
 <?php
-	if (isset($_POST['jour'])) {
+	if (isset($_POST['submit'])) {
 		if (!empty($_POST['jour']) AND !empty($_POST['mois']) AND !empty($_POST['annee'])) {
 			$date=$_POST['annee'] . '-' . $_POST['mois'] . '-' . $_POST['jour'];
 		}
