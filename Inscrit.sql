da935abc72839b9b7a30fc273c1fba5f9cf7be9b
NicolasTYSSANDIER <nicolas_881@hotmail.fr>
2016-05-30 16:33:42 +0200

    Update Inscrit.sql

--- Inscrit.sql
+++ Inscrit.sql
@@ -1,14 +1,20 @@
 -- phpMyAdmin SQL Dump
--- version 4.4.10
+-- version 4.5.2
 -- http://www.phpmyadmin.net
 --
--- Client :  localhost
--- Généré le :  Lun 23 Mai 2016 à 15:16
--- Version du serveur :  5.5.42
--- Version de PHP :  5.6.10
+-- Client :  127.0.0.1
+-- Généré le :  Lun 30 Mai 2016 à 14:24
+-- Version du serveur :  5.7.9
+-- Version de PHP :  5.6.16
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Base de données :  `agon`
@@ -17,37 +23,23 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Structure de la table `Inscrit`
+-- Structure de la table `inscrit`
 --
 
-CREATE TABLE `Inscrit` (
-  `id` int(11) NOT NULL,
-  `nom_user` varchar(20) NOT NULL,
-  `prenom_user` varchar(20) NOT NULL,
-  `date_de_naissance` varchar(60) NOT NULL,
+DROP TABLE IF EXISTS `inscrit`;
+CREATE TABLE IF NOT EXISTS `inscrit` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nom` varchar(20) NOT NULL,
+  `prenom` varchar(20) NOT NULL,
+  `date_naissance` date NOT NULL,
   `sexe` varchar(50) NOT NULL,
   `departement` varchar(50) NOT NULL,
   `email` varchar(60) NOT NULL,
-  `pseudo` varchar(40) NOT NULL,
-  `password` varchar(40) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `mdp` varchar(40) NOT NULL,
+  `sport` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=119 DEFAULT CHARSET=latin1;
 
---
--- Index pour les tables exportées
---
-
---
--- Index pour la table `Inscrit`
---
-ALTER TABLE `Inscrit`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT pour les tables exportées
---
-
---
--- AUTO_INCREMENT pour la table `Inscrit`
---
-ALTER TABLE `Inscrit`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
