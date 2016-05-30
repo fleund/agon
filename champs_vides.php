34555d824badfcffea232be240dd2c36f4107c22
NicolasTYSSANDIER <nicolas_881@hotmail.fr>
2016-05-30 19:36:13 +0200

    Update champs_vides.php

--- champs_vides.php
+++ champs_vides.php
@@ -1,7 +1,7 @@
 <?php
-	for ($i=0; $i<=$nb_champs; $i++) {
-        $nom_champ[$i]=htmlentities(trim($_POST[$nom_champ[$i]]));
-        if (empty($nom_champ[$i])) { // On vérifie qu'aucun champ n'est vide
+	for ($i=0; $i<=count($nom_champ)-1; $i++) {
+        $contenu_champ[$i]=htmlentities(trim($_POST[$nom_champ[$i]]));
+        if (empty($contenu_champ[$i])) { // On vérifie qu'aucun champ n'est vide
 	        $vide=true;
 	        break;
         }
