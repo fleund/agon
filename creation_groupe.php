67d2ecb783a522a436dc8e11fae53486f9917add
NicolasTYSSANDIER <nicolas_881@hotmail.fr>
2016-05-30 22:58:15 +0200

    Update creation_groupe.php

--- creation_groupe.php
+++ creation_groupe.php
@@ -10,43 +10,41 @@
     </head>
 
     <body>
-        <ul>
-            <a href="index.html">Accueil</a>
-            <a href="forum.html">Forum</a>
-            <a href="groupe.html">Groupe</a>
-            <a href="mon_espace.html">Mon espace</a>
-            <a href="deconnexion.html">Deconnexion</a>
-        </ul></br>
-        
+        <?php
+            if(isset($_POST['submit'])) {
+                $champ=array('nom', 'description', 'sport', 'statut', 'departement', 'image', 'membres_max');
+                include('champs_vides.php');
+                if (!isset($vide)) {
+                    for ($i=0; $i<=count($champ)-1; $i++) {$pre_array[$champ[$i]]=$contenu[$champ[$i]];}
+                    $req = $bdd->prepare('INSERT INTO groupe(nom, description, sport, statut, departement, image, inscrits, membres_max) VALUES(:nom, :description, :sport, :statut, :departement, :image, 1, :membres_max)');
+                    $req->execute($pre_array);
+                    $reponse = $bdd->query('SELECT MAX(id) AS id FROM groupe');
+                    $donnees = $reponse->fetch();
+                    header('Location: groupe.php?id=' . $donnees['id']);
+                }
+                else {echo '<strong class="erreur">Veuillez renseigner tous les champs indiqués.</strong></br>';}
+            }
+        ?>
         <form action="creation_groupe.php" method="post" class="champ_recherche">
-            <p>Nom du groupe : <input type="text" name="nom" class="l300"></p>
-            <p>Description (facultatif) :</br><textarea name="description" rows="8" maxlength="1000" class="l300" placeholder="Décrivez votre groupe ici..."></textarea></p>
+            <p>Nom du groupe : <input type="text" name="nom" class="l300"<?php if (isset($contenu['nom'])) {echo ' value="' . $contenu['nom'] . '"';} ?>></p>
+            <p>Description (facultatif) :</br><textarea name="description" rows="8" maxlength="1000" class="l300" placeholder="Décrivez votre groupe ici..."><?php if (isset($contenu['description'])) {echo $contenu['description'];} ?></textarea></p>
             <p>Sport principal : <?php include('liste_sports.php') ?></p>
             <p>Statut : </br>
                 <select name='statut'>
                     <option>Public</option>
-                    <option>Privé</option>
+                    <option
+                    <?php if (isset($contenu['statut'])) {
+                        if ($contenu['statut']=='Privé') {echo ' selected="selected"';}
+                    } ?>
+                    >Privé</option>
                 </select>
             </p>
             <p>Département : <?php include('liste_departements.php') ?></p>
-            <p>Image du groupe : <input type="text" name="image"></br><a href='http://www.hostingpics.net/' target="_blank">Lien vers un uploader de photos</a></p>
-            <p>Nombre maximum de membres : <input type="number" min="5" max="200" value="100" name="membres_max" maxlength="3"></p>
+            <p>Image du groupe : <input name="image"<?php if (isset($contenu['image'])) {echo ' value="' . $contenu['image'] . '"';} ?>>
+            </br><a href='http://www.hostingpics.net/' target="_blank">Lien vers un uploader de photos</a></p>
+            <p>Nombre maximum de membres :
+            <input type="number" min="5" max="200" name="membres_max" maxlength="3"<?php if (isset($contenu['membres_max'])) {echo ' value="' . $contenu['membres_max'] . '"';} ?>></p>
             <input id="search-btn" type="submit" value="Créer la compétition" name="submit"> 
         </form>
-        <?php
-            if(isset($_POST['nom'])) {
-                $req = $bdd->prepare('INSERT INTO groupe(nom, description, sport, statut, departement, image, membres, membres_max) VALUES(:nom, :description, :sport, :statut, :departement, :image, 1, :membres_max)');
-                $req->execute(array(
-                    'nom' => $_POST['nom'],
-                    'description' => $_POST['description'],
-                    'sport' => $_POST['sport'],
-                    'statut' => $_POST['statut'],
-                    'departement' => $_POST['departement'],
-                    'image' => $_POST['image'],
-                    'membres_max' => $_POST['membres_max']
-                ));
-                echo 'Le groupe a été créé.';
-            }
-        ?>
     </body>
 </html>
