6d0fad7e9927976bdc394dff08edec0e457f4116
NicolasTYSSANDIER <nicolas_881@hotmail.fr>
2016-05-30 16:29:39 +0200

    Update creation_compet.php

--- creation_compet.php
+++ creation_compet.php
@@ -11,28 +11,34 @@
 
     <body>
         <form action="creation_compet.php" method="post" class="champ_recherche">
-            <p>Nom de la compétition : <input type="text" name="nom" class="l300"></p>
+            <p>Nom de la compétition : <input type="text" name="nom" class="l300" <?php if (isset($_POST['nom'])) {echo ' value="' . $_POST['nom'] . '" ';} ?>></p>
             <p>Sport principal : <?php include('liste_sports.php') ?></p>
             <p>Date : <?php include('entrer_date.php') ?></p>
-            <p>Lieu : <textarea name="lieu" maxlength="100" rows="3" placeholder="Exemple : Club de Football de Versailles, Gymnase Saint-Germain Paris 6è..." class="l300"></textarea></p>
+            <p>Lieu : <textarea name="lieu" maxlength="100" rows="3" placeholder="Exemple : Club de Football de Versailles, Gymnase Saint-Germain Paris 6è..." class="l300"><?php if (isset($_POST['lieu'])) {echo $_POST['lieu'];} ?></textarea></p>
             <p>Département : <?php include('liste_departements.php') ?></p>
-            <p>Nombre de places total : <input type="number" name="places" maxlength="6"></p>
+            <p>Nombre de places total : <input type="number" name="places" maxlength="6" <?php if (isset($_POST['places'])) {echo ' value="' . $_POST['places'] . '" ';} ?>></p>
             <input id="search-btn" type="submit" value="Créer la compétition" name="submit"> 
         </form>
         <?php
-            if(isset($_POST['nom'])) {
-                $groupe='groupe'; // En attendant qu'on aie des vrais groupes créés
-                $req = $bdd->prepare('INSERT INTO liste_compets(nom, sport, date, lieu, departement, groupe, places_total, inscrits) VALUES(:nom, :sport, :date, :lieu, :departement, :groupe, :places_total, 0)');
-                $req->execute(array(
-                    'nom' => $_POST['nom'],
-                    'sport' => $_POST['sport'],
-                    'date' => $date,
-                    'lieu' => $_POST['lieu'],
-                    'departement' => $_POST['departement'],
-                    'groupe' => $groupe,
-                    'places_total' => $_POST['places'],
-                ));
-                echo 'La compétition a été créée.';
+            if(isset($_POST['submit'])) {
+                $nom_champ=array('nom', 'sport', 'lieu', 'departement', 'places');
+                $nb_champs=4;
+                include('champs_vides.php');
+                if (!isset($vide)) {
+                    $groupe='groupe'; // En attendant qu'on aie des vrais groupes créés
+                    $req = $bdd->prepare('INSERT INTO liste_compets(nom, sport, date, lieu, departement, groupe, places_total, inscrits) VALUES(:nom, :sport, :date, :lieu, :departement, :groupe, :places_total, 0)');
+                    $req->execute(array(
+                        'nom' => $_POST['nom'],
+                        'sport' => $_POST['sport'],
+                        'date' => $date,
+                        'lieu' => $_POST['lieu'],
+                        'departement' => $_POST['departement'],
+                        'groupe' => $groupe,
+                        'places_total' => $_POST['places']
+                    ));
+                    echo 'La compétition a été créée.';
+                }
+                else {echo '<strong class="erreur">Veuillez renseigner tous les champs indiqués.</strong></br>';}
             }
         ?>
     </body>
