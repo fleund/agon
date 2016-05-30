32c9946ccf221d1990970ba48e4ab6868df41a6b
NicolasTYSSANDIER <nicolas_881@hotmail.fr>
2016-05-30 16:27:48 +0200

    Update entrer_date.php

--- entrer_date.php
+++ entrer_date.php
@@ -1,10 +1,11 @@
-<input type="number" name="jour" maxlength="2" placeholder="JJ" class="champ_date"> / 
-<input type="number" name="mois" maxlength="2" placeholder="MM" class="champ_date"> / 
-<input type="number" name="annee" maxlength="4" placeholder="AAAA" class="champ_date">
+<input type="number" name="jour" maxlength="2" placeholder="JJ" class="champ_date"<?php if (isset($_POST['jour'])) {echo ' value="' . $_POST['jour'] . '" ';} ?>> / 
+<input type="number" name="mois" maxlength="2" placeholder="MM" class="champ_date"<?php if (isset($_POST['mois'])) {echo ' value="' . $_POST['mois'] . '" ';} ?>> / 
+<input type="number" name="annee" maxlength="4" placeholder="AAAA" class="champ_date"<?php if (isset($_POST['annee'])) {echo ' value="' . $_POST['annee'] . '" ';} ?>>
 <?php
 	if (isset($_POST['jour'])) {
 		if (!empty($_POST['jour']) AND !empty($_POST['mois']) AND !empty($_POST['annee'])) {
 			$date=$_POST['annee'] . '-' . $_POST['mois'] . '-' . $_POST['jour'];
 		}
+		else {$vide=true;}
 	}
 ?>
