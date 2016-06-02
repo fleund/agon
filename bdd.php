25e39cb668567215779f422018aaee01469b906c
NicolasTYSSANDIER <nicolas_881@hotmail.fr>
2016-06-03 01:05:47 +0200

    Update bdd.php

--- bdd.php
+++ bdd.php
@@ -1,4 +1,9 @@
 <?php
     try {$bdd = new PDO('mysql:host=localhost;dbname=agon;charset=utf8', 'root', '');}
     catch (Exception $e) {die('Erreur : ' . $e->getMessage());}
+    session_start();
+    if (isset($_POST['deconnexion'])) {
+    	session_destroy();
+    	unset($_SESSION);
+    }
 ?>
