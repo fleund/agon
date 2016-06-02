3e7885960431f9179b9bfa2408352f2ebb7a046b
NicolasTYSSANDIER <nicolas_881@hotmail.fr>
2016-06-03 01:04:27 +0200

    Update liste_sports.php

--- liste_sports.php
+++ liste_sports.php
@@ -1,13 +1,18 @@
-<select name="sport" class="critere">
-    <option value="">Choisir un sport</option>
-    <?php
-        $reponse = $bdd->query('SELECT * FROM liste_sports ORDER BY nom');
-        while ($donnees = $reponse->fetch()) {
-            echo '<option';
-            if (isset($_POST['sport'])) {
-                if ($donnees['nom']==$_POST['sport']) {echo ' selected="selected"';}
+<div class="champ">
+    <select name="sport" class="critere">
+        <option value="">Choisir un sport</option>
+        <?php
+            $reponse = $bdd->query('SELECT * FROM liste_sports ORDER BY nom');
+            while ($donnees = $reponse->fetch()) {
+                echo '<option';
+                if (isset($_POST['sport'])) {
+                    if ($donnees['nom']==$_POST['sport']) {echo ' selected="selected"';}
+                }
+                if (isset($_GET['sport'])) {
+                    if ($donnees['nom']==$_GET['sport']) {echo ' selected="selected"';}
+                }
+                echo '>' . $donnees['nom'] . '</option>';
             }
-            echo '>' . $donnees['nom'] . '</option>';
-        }
-    ?>
-</select>
+        ?>
+    </select>
+</div>
