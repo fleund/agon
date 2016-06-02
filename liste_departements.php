91ba2f72916c51302361572bb41940255b7073dd
NicolasTYSSANDIER <nicolas_881@hotmail.fr>
2016-06-03 01:04:08 +0200

    Update liste_departements.php

--- liste_departements.php
+++ liste_departements.php
@@ -1,13 +1,18 @@
-<select name="departement" class="critere">
-	<option value="">Choisir un département</option>
-	<?php
-	    $reponse = $bdd->query('SELECT * FROM departement ORDER BY numero');
-	    while ($donnees = $reponse->fetch()) {
-            echo '<option value="' . $donnees['numero'] . ' - ' . $donnees['nom'] . '"';
-            if (isset($_POST['departement'])) {
-                if ($donnees['numero'] . ' - ' . $donnees['nom']==$_POST['departement']) {echo ' selected="selected"';}
+<div class="champ">
+    <select name="departement" class="critere">
+    	<option value="">Choisir un département</option>
+    	<?php
+    	    $reponse = $bdd->query('SELECT * FROM departement ORDER BY numero');
+    	    while ($donnees = $reponse->fetch()) {
+                echo '<option value="' . $donnees['numero'] . ' - ' . $donnees['nom'] . '"';
+                if (isset($_POST['departement'])) {
+                    if ($donnees['numero'] . ' - ' . $donnees['nom']==$_POST['departement']) {echo ' selected="selected"';}
+                }
+                if (isset($_GET['departement'])) {
+                    if ($donnees['numero'] . ' - ' . $donnees['nom']==$_GET['departement']) {echo ' selected="selected"';}
+                }
+                echo '>' . $donnees['numero'] . ' - ' . $donnees['nom'] . '</option>';
             }
-            echo '>' . $donnees['numero'] . ' - ' . $donnees['nom'] . '</option>';
-        }
-	?>
-</select>
+    	?>
+    </select>
+</div>
