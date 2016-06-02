d480a1a663f7fae3f51728e7d1eafb0d5e13d09e
NicolasTYSSANDIER <nicolas_881@hotmail.fr>
2016-06-03 00:16:14 +0200

    Update Accueil.php

--- Accueil.php
+++ Accueil.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 
-<?php include('bdd.php') ?>
+<?php include('bdd.php');?>
 
 <html>
     <head>
@@ -14,27 +14,54 @@
     
     <body>
     <div id="conteneur">
-		<div class="element">
-			<img class="Logo" src="Logo.png" alt="Logo Agon" />
-		</div>
-		<div class="element">
-			<form method="POST" action="Edition page d'accueil.php"><input id="edition_acceuil" type="submit" value="Editer accueil"/></form>
-			<form method="POST" action="creation_groupe.php"><input id="creation_groupe" type="submit" value="Créer groupe"/></form>
-			<form method="POST" action=".php"><input id="connexion" type="submit" value="Connexion" /></form>
-			<form method="POST" action="formulaire_inscription.php"><input id="inscription" type="submit" value="Inscription" /></form>
-		</div>
+        <div class="element"><img class="Logo" src="Logo.png" alt="Logo Agon"></div>
+        <div class="element">
+            <?php
+                if (isset($_POST['connexion'])) {
+                    $champ=array('email', 'mdp');
+                    include('champs_vides.php');
+                    if (!isset($vide)) {
+                        $reponse = $bdd->prepare('SELECT * FROM inscrit WHERE email = :email AND mdp = :mdp');
+                        $reponse->execute(array(
+                            'email' => $_POST['email'],
+                            'mdp' => $_POST['mdp']
+                        ));
+                        $donnees = $reponse->fetch();
+                        if (!empty($donnees)) {
+                            $_SESSION['prenom']=$donnees['prenom'];
+                            $_SESSION['nom']=$donnees['nom'];
+                            $_SESSION['id']=$donnees['id'];
+                        }
+                        else {echo '<strong class="erreur">L\'adresse e-mail ou le mot de passe est invalide.</strong>';}
+                    }
+                    else {echo '<strong class="erreur">Veuillez renseigner une adresse e-mail et un mot de passe.</strong>';}
+                }
+                if (empty($_SESSION)) {
+                    echo '<form method="post" action="Accueil.php">
+                    <input name="email" type="email" placeholder="Adresse e-mail"';
+                    if (isset($_POST['email'])) {echo ' value="' . $_POST['email'] . '"';}
+                    echo '><input name="mdp" type="password" placeholder="Mot de passe">
+                    <input type="submit" name="connexion" value="Se connecter" class="bouton_accueil">
+                    </form><a href="formulaire_inscription.php" class="bouton_accueil">S\'inscrire</a>';
+                }
+                else {
+                    echo '<a href="creation_groupe.php" class="bouton_accueil">Créer un groupe</a>
+                    <a href="Edition page d\'accueil.php" class="bouton_accueil">Édition de la page d\'accueil</a>';
+                    echo '<a href="Profil.php?id=' . $_SESSION['id'] . '" class="bouton_accueil">' . $_SESSION['prenom'] . ' ' . $_SESSION['nom'] . '</a>';
+                    echo '<form method="post" action="Accueil.php"><input type="submit" name="deconnexion" value="Déconnexion" class="bouton_accueil"></form>';
+                }
+            ?>
+	    </div>
+        <div class="element">
+            <div id="onglets_accueil">
+                <div><a href="Accueil.php">Accueil</a></div>
+                <div><a href="Index des forums.php">Forum</a></div>
+                <div><a href="resultats_recherche.php?avancee=false&amp;groupe=oui">Groupes</a></div>
+                <?php if (!empty($_SESSION)) {echo '<div><a href="">Mon espace</a></div>';} ?>
+            </div>   
+	    </div>
 		<div class="element">    
-		</div>
-		<div class="element">
-			<div id="onglets_accueil">
-				<div><a href="Accueil.php">Accueil</a></div>
-				<div><a href="Index des forums.php">Forum</a></div>
-				<div><a href="groupe.php">Groupes</a></div> 
-				<div><a href="">Mon espace</a></div>
-			</div>   
-		</div>
-		<div class="element">
-		</div>
+        </div>
         <div class="element">
                 <p class="message_important"> SPORTIFS DE TOUS LES JOURS, VENEZ !</p>
                 <li>FORME TON GROUPE</li>
@@ -54,7 +81,7 @@
             $_GET['avancee']='false'; // On initialise la recherche avancée en mode "désactivée"
             include('search.php');
         ?> 
-		
+
     </div>
     <div id='bloc_galerie'>
         <div id="galerie">
