3e8378f711bc41cde255cf70b5a88513855cc20f
NicolasTYSSANDIER <nicolas_881@hotmail.fr>
2016-06-03 14:46:49 +0200

    Update Inscrit.sql

--- Inscrit.sql
+++ Inscrit.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  127.0.0.1
--- Généré le :  Lun 30 Mai 2016 à 14:24
+-- Généré le :  Ven 03 Juin 2016 à 12:45
 -- Version du serveur :  5.7.9
 -- Version de PHP :  5.6.16
 
@@ -37,8 +37,21 @@ CREATE TABLE IF NOT EXISTS `inscrit` (
   `email` varchar(60) NOT NULL,
   `mdp` varchar(40) NOT NULL,
   `sport` varchar(255) NOT NULL,
+  `nombre_groupe` int(11) NOT NULL,
+  `date_inscription` date NOT NULL,
+  `id_groupe` int(11) NOT NULL,
+  `num_tel` int(10) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=119 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=140 DEFAULT CHARSET=latin1;
+
+--
+-- Contenu de la table `inscrit`
+--
+
+INSERT INTO `inscrit` (`id`, `nom`, `prenom`, `date_naissance`, `sexe`, `departement`, `email`, `mdp`, `sport`, `nombre_groupe`, `date_inscription`, `id_groupe`, `num_tel`) VALUES
+(1, 'Admin', '', '2016-06-03', '', '', 'nicolas_881@hotmail.fr', 'admin', '', 0, '2016-06-03', 0, 0),
+(137, 'Tyssandier', 'Nicolas', '1900-01-01', '', '', 'tyssandiernicolas@gmail.com', 'abc', 'Badminton', 0, '0000-00-00', 0, 0),
+(138, 'Cothenet', 'Côme', '1900-01-01', '', '', 'moche@gmail.com', 'jesuisleplusmoche', 'Danse', 0, '0000-00-00', 0, 0);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
