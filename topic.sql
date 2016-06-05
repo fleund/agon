83a40dd1e7cd3fbdcecc93124a909571d64cb0bb
fleund <come.cothenet@hotmail.fr>
2016-06-05 23:52:21 +0200

    Add files via upload

--- topic.sql
+++ topic.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  127.0.0.1
--- Généré le :  Dim 05 Juin 2016 à 19:46
+-- Généré le :  Dim 05 Juin 2016 à 21:51
 -- Version du serveur :  5.7.9
 -- Version de PHP :  5.6.15
 
@@ -37,8 +37,9 @@ CREATE TABLE IF NOT EXISTS `topic` (
   `nom_auteur` varchar(100) NOT NULL,
   `premier_message` text NOT NULL,
   `nombre_reponses` int(11) NOT NULL DEFAULT '0',
+  `id_auteur` int(11) NOT NULL,
   PRIMARY KEY (`id_topic`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin7;
+) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=latin7;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
