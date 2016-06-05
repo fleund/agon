2cb0e3e1035f6b2f809cf6767d523f2494fc6e73
NicolasTYSSANDIER <nicolas_881@hotmail.fr>
2016-06-05 20:29:25 +0200

    Update groupe.php

--- groupe.php
+++ groupe.php
@@ -1,59 +1,37 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
+
 <html>
     <head>
         <title>Groupe</title>
-		<meta charset="UTF-8">
+        <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="Accueil.css"/>
+        <link rel="stylesheet" href="Accueil.css">
     </head>
-    <body>
-        
-        <?php 
-                include ('bdd.php');
-				include('header.php');
-                
-                $req = $bdd->prepare('SELECT * FROM groupe WHERE id = :id '); // Table de la liste des sports en en-tÃªte
-                    $req->execute(array('id' => $_GET['id'] ));
-                    $donnees = $req->fetch();
-            
-            ?>
-			
-				<div id="barre_bleue_gauche">
-				</div>
-				<div id="barre_bleue_droite">
-				</div>
-				<div id="barre_mauve_gauche">
-				</div>
-				<div id="barre_mauve_droite">
-				</div>
-				
-                <div class = "titremarge" >
-                    <h1>Groupe : <?php echo $donnees['nom']; ?></h1>
-                </div>   
-        
-                <div class = "marge" >
-                    <img src=<?php echo $donnees['image'];?> alt="Profil" title="image_profil_<?php echo $donnees['nom']; ?>"/>
-                </div>
-                <div class = "bordure" >
-		
-                    <h2>Informations</h2>
-                    <ul>
-                        <li>Statut :<?php echo $donnees['statut']; ?></li> </br>
-                        <li>Description :<?php echo $donnees['description']; ?></li> </br>
-                        <li>Nombre maximum de membres :<?php echo $donnees['membres_max']; ?></li></br>
-                        <li>Sport :<?php echo $donnees['sport']; ?></li> </br
-                        <li>Département :<?php echo $donnees['departement']; ?></li> </br>
-                </div>
-            
-            
-            <div class="bouton">
-                <input type="submit" value="Modifier les informations" class ="agrandir_bouton">
-            </div>
 
-	</body>
+    <body>
+        <?php
+            include ('bdd.php');
+            include('header.php');
+            $req = $bdd->prepare('SELECT * FROM groupe WHERE id = :id '); // Table de la liste des sports en en-tÃªte
+            $req->execute(array('id' => $_GET['id'] ));
+            $donnees = $req->fetch();
+        ?>
+        <div id="barre_bleue_gauche"></div>
+        <div id="barre_bleue_droite"></div>
+        <div id="barre_mauve_gauche"></div>
+        <div id="barre_mauve_droite"></div>
+        <div class="titremarge"><h1>Groupe : <?php echo $donnees['nom']; ?></h1></div>
+        <div class="marge"><img src=<?php echo $donnees['image'];?> alt="Profil" title=<?php echo 'image_profil_' . $donnees['nom']; ?>></div>
+        <div class="bordure">
+            <h2>Informations</h2>
+            <ul>
+                <li>Statut : <?php echo $donnees['statut']; ?></li></br>
+                <li>Description : <?php echo $donnees['description']; ?></li></br>
+                <li>Nombre maximum de membres : <?php echo $donnees['membres_max']; ?></li></br>
+                <li>Sport : <?php echo $donnees['sport']; ?></li></br>
+                <li>Département : <?php echo $donnees['departement']; ?></li></br>
+            </ul>
+        </div>
+        <div class="bouton"><input type="submit" value="Modifier les informations" class="agrandir_bouton"></div>
+    </body>
 </html>
