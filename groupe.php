772c42ad81767a76942d062063c47b3d0af3cad6
NicolasTYSSANDIER <nicolas_881@hotmail.fr>
2016-06-06 04:32:52 +0200

    Update groupe.php

--- groupe.php
+++ groupe.php
@@ -12,9 +12,29 @@
         <?php
             include ('bdd.php');
             include('header.php');
+            if (isset($_POST['detruire'])) {
+                $req = $bdd->prepare('DELETE FROM appartenance WHERE id_g = :id_g');
+                $req->execute(array('id_g' => $_GET['id']));
+                $req = $bdd->prepare('DELETE FROM groupe WHERE id = :id');
+                $req->execute(array('id' => $_GET['id']));
+            }
+            if (isset($_POST['quitter'])) {
+                $req = $bdd->prepare('DELETE FROM appartenance WHERE id_g = :id_g AND id_i = :id_i');
+                $req->execute(array(
+                    'id_g' => $_GET['id'],
+                    'id_i' => $_SESSION['id']
+                ));
+            }
+            if (isset($_POST['inscrire'])) {
+                $req = $bdd->prepare('INSERT INTO appartenance(id_g, id_i) VALUES (:id_g, :id_i)');
+                $req->execute(array(
+                    'id_g' => $_GET['id'],
+                    'id_i' => $_SESSION['id']
+                ));
+            }
             $req = $bdd->prepare('SELECT * FROM groupe WHERE id = :id');
             $req->execute(array('id' => $_GET['id']));
-            $donnees = $req->fetch();
+            $donnees = $req->fetch(); // On récupère toutes les infos du groupe
         ?>
         <div id="barre_bleue_gauche"></div>
         <div id="barre_bleue_droite"></div>
@@ -32,8 +52,28 @@
                 <li>Département : <?php echo $donnees['departement']; ?></li></br>
             </ul>
         </div>
-        <?php // Si l'utilisateur est le leader du groupe, il peut modifier les informations
-            if ($donnees['id_leader']==$_SESSION['id']) {echo '<button type="button" class="agrandir_bouton"><a href="edition_groupe.php?id=' . $_GET['id'] . '">Modifier les informations</a></button>';}
+        <?php
+            if (!empty($_SESSION)) {
+                $reponse = $bdd->prepare('SELECT * FROM appartenance WHERE id_g = :id_g AND id_i = :id_i');
+                $reponse->execute(array(
+                    'id_g' => $_GET['id'],
+                    'id_i' => $_SESSION['id']
+                    ));
+                $donnees2 = $reponse->fetch();
+                if (!empty($donnees2)) { // Si l'utilisateur est membre du groupe
+                    echo '</br><strong>Vous êtes actuellement membre de ce groupe.</strong>';
+                    if ($donnees['id_leader']==$_SESSION['id']) { // Si l'utilisateur est leader du groupe
+                        echo '<button type="button" class="agrandir_bouton"><a href="edition_groupe.php?id=' . $_GET['id'] . '">Modifier les informations</a></button>';
+                        echo '<form method="post" action="groupe.php?id=' . $_GET['id'] . '"><input type="submit" class="agrandir_bouton" value="Détruire ce groupe" name="detruire"></form>';
+                    }
+                    else {
+                        echo '<form method="post" action="groupe.php?id=' . $_GET['id'] . '"><input type="submit" class="agrandir_bouton" value="Quitter ce groupe" name="quitter"></form>';
+                    }
+                }
+                elseif ($donnees['statut']=='Public') { // Si l'utilisateur n'est pas membre du groupe, mais il s'agit d'un groupe public
+                    echo '<form method="post" action="groupe.php?id=' . $_GET['id'] . '"><input type="submit" class="agrandir_bouton" value="S\'inscrire à ce groupe" name="inscrire"></form>';
+                }
+            }
         ?>
     </body>
 </html>
