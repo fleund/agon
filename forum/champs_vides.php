1516a2ffac230fffd880147f491f2e21a1b67dc3
fleund <come.cothenet@hotmail.fr>
2016-06-06 18:13:35 +0200

    Delete champs_vides.php

--- forum/champs_vides.php
+++
@@ -1,6 +0,0 @@
-<?php
-	for ($i=0; $i<=count($champ)-1; $i++) {
-        $contenu[$champ[$i]]=strip_tags(trim($_POST[$champ[$i]]));
-        if (empty($contenu[$champ[$i]])) {$vide=true;} // On vérifie qu'aucun champ n'est vide
-    }
-?>
