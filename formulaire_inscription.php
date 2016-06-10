0b3d81fd269bc808ba4b1335b96a254d29b20191
katiafiroud <katia_firoud@hotmail.fr>
2016-06-10 11:04:55 +0200

    Update formulaire_inscription.php

--- formulaire_inscription.php
+++ formulaire_inscription.php
@@ -23,26 +23,29 @@
               include('champs_vides.php');
               if (!isset($vide)) {
                 if ($contenu['mdp']==$contenu['confirmation']) {
-                $req = $bdd->prepare('INSERT INTO inscrit(nom, prenom, date_naissance, sexe, departement, email, mdp, sport, date_inscription, num_tel) VALUES(:nom, :prenom, :date_naissance, :sexe, :departement, :email, :mdp, :sport, :date_inscription, 0)'); // On inscrit le membre
-                $req->execute(array(
-                  'nom' => $contenu['nom'],
-                  'prenom' => $contenu['prenom'],
-                  'date_naissance' => '1900-01-01', // Date par défaut
-                  'sexe' => '',
-                  'departement' => '',
-                  'email' => $contenu['email'],
-                  'mdp' => md5($contenu['mdp']),
-                  'sport' => $contenu['sport'],
-                  'date_inscription' => date('Y-m-d')
-                ));
-                $_SESSION['nom']=$contenu['nom'];
-                $_SESSION['prenom']=$contenu['prenom'];
-                $req = $bdd->prepare('UPDATE liste_sports SET nb_membres=nb_membres+1 WHERE nom=:nom'); // On incrémente le nombre de gens qui pratiquent ce sport
-                $req->execute(array('nom' => $_POST['sport']));
-                $reponse = $bdd->query('SELECT MAX(id) AS id FROM inscrit');
-                $donnees = $reponse->fetch();
-                $_SESSION['id']=$donnees['id'];
-                header('Location: Profil.php?id=' . $_SESSION['id']);;
+                  if(filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)){
+                    $req = $bdd->prepare('INSERT INTO inscrit(nom, prenom, date_naissance, sexe, departement, email, mdp, sport, date_inscription, num_tel) VALUES(:nom, :prenom, :date_naissance, :sexe, :departement, :email, :mdp, :sport, :date_inscription, 0)'); // On inscrit le membre
+                    $req->execute(array(
+                    'nom' => $contenu['nom'],
+                    'prenom' => $contenu['prenom'],
+                    'date_naissance' => '1900-01-01', // Date par défaut
+                    'sexe' => '',
+                    'departement' => '',
+                    'email' => $contenu['email'],
+                    'mdp' => md5($contenu['mdp']),
+                    'sport' => $contenu['sport'],
+                    'date_inscription' => date('Y-m-d')
+                    ));
+                    $_SESSION['nom']=$contenu['nom'];
+                    $_SESSION['prenom']=$contenu['prenom'];
+                    $req = $bdd->prepare('UPDATE liste_sports SET nb_membres=nb_membres+1 WHERE nom=:nom'); // On incrémente le nombre de gens qui pratiquent ce sport
+                    $req->execute(array('nom' => $_POST['sport']));
+                    $reponse = $bdd->query('SELECT MAX(id) AS id FROM inscrit');
+                    $donnees = $reponse->fetch();
+                    $_SESSION['id']=$donnees['id'];
+                    header('Location: Profil.php?id=' . $_SESSION['id']);;
+                  }
+                  else {echo '<strong class="erreur">Veuillez saisir une adresse mail valide.</strong></br>';}
                 }
                 else {echo '<strong class="erreur">Les deux mots de passe doivent être identiques.</strong></br>';}
               }
